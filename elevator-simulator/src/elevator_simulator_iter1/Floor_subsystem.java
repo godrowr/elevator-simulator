73cf577efc9a719b93478d8b86960a940298fec3
Andrew <andrewcowan19@hotmail.com>
2020-02-01 23:04:23 -0500

    Reversal

--- elevator-simulator/src/elevator_simulator_iter1/Floor_subsystem.java
+++ elevator-simulator/src/elevator_simulator_iter1/Floor_subsystem.java
@@ -73,11 +73,47 @@ public class Floor_subsystem implements Runnable{
 			//System.out.println("Here");
 			sendInfoToScheduler(buttons);
 		}
-		
-		
-		
+
 	}
 	
+	/*
+	 * This method is used for testing purposes only
+	 */
+	public List<Button> inputFile() {
+		
+		List<Button> safecase = new ArrayList<Button>();
+		
+		try {
+			List<String[]> morelines = new ArrayList<String[]>();
+			InputStream in = Floor_subsystem.class.getResourceAsStream("inputFile.txt");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+			
+			String l;
+			while ((l = reader.readLine()) != null) {
+				//System.out.println(l);
+				String[] splited = l.split("\\s+");
+				morelines.add(splited);
+			}
+            
+			List<Button> buttons = new ArrayList<Button>();
+			FloorButton floor;
+			ElevatorButton destination;
+			for(String[] line : morelines) {
+				floor = new FloorButton(line[0], Integer.parseInt(line[1]), line[2]);
+				destination = new ElevatorButton(Integer.parseInt(line[1]), Integer.parseInt(line[3]));
+				buttons.add(floor);
+				buttons.add(destination);
+				//System.out.println("Here");
+				return buttons;
+			}
+		} catch ( IOException e1) {
+			// TODO Auto-generated catch block URISyntaxException |
+			e1.printStackTrace();
+			return safecase;
+		}
+		
+		return safecase;
+	}
 }
 
 /**
