6a8f5e00fc35a10232133278be0511e69c2db85e
Ryan Frohar <ryan.frohar@gmail.com>
2020-03-05 21:43:28 -0500

    Created elevatorInfo class

---
+++ elevator-simulator/src/elevator_simulator_iter1/ElevatorInfo.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class ElevatorInfo{
+	private int dest;
+	private int elevatorNo;
+	private int current;
+	
+	public ElevatorInfo(int elevatorNo, int current, int dest) {
+		this.elevatorNo=elevatorNo;
+		this.current=current;
+		this.dest=dest;
+	}
+	
+	public int[] getElements() {
+		return (new int{this.dest, this.current, this.elevatorNo});
+	}
+}
