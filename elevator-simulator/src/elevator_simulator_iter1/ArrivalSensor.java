7e369efefe7547328b6cd1bd9243666240a5e2d3
godrowr <35468510+godrowr@users.noreply.github.com>
2020-03-07 19:37:23 -0500

    Readded Arrival Sensor.

---
+++ elevator-simulator/src/elevator_simulator_iter1/ArrivalSensor.java
@@ -0,0 +1,14 @@
+
+package elevator_simulator_iter1;
+
+
+public class ArrivalSensor {
+	private int curFloor;
+	
+	public ArrivalSensor() {}
+	
+	public int getCurFloor() {
+		return curFloor;
+	}
+
+}
